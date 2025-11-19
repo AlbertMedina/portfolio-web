f1983ce1212c37de9a6c78d8a3e264314eda7468
Albert Medina <albertmecu@gmail.com>
2025-11-19 19:46:18 +0100

    feat: added necessary stuff to main (empty components by now)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+
+import i18n from '../i18n.config';
+import './index.css';
+
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
+import Experience from './components/Experience';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </I18nextProvider>
+  </StrictMode>
+);

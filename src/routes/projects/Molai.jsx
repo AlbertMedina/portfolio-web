4c0053d3edcd7cf1c83a441124a6d08c8f280994
Albert Medina <albertmecu@gmail.com>
2026-03-10 12:58:23 +0100

    feat: added new project pages + improved texts

--- src/routes/projects/Molai.jsx
+++ src/routes/projects/Molai.jsx
@@ -1,1 +1,40 @@
-export default function Molai() {}
+import { useTranslation } from "react-i18next";
+
+import styles from "./Project.module.css";
+
+import logo from "../../assets/images/projects/molai/logo.webp";
+import image1 from "../../assets/images/projects/molai/image-1.webp";
+
+export default function Molai() {
+  const { t } = useTranslation();
+  return (
+    <section className={styles.project}>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <div className={styles.titleLeft}>
+            <img src={logo} alt="Molai Logo" />
+            <h1>{t("projects.molai.title")}</h1>
+          </div>
+          <div className={styles.buttons}></div>
+        </div>
+        <div className={styles.info}>
+          <img src={image1} alt={t("projects.molai.title")} />
+          <div className={styles.text}>
+            <p>{t("projects.molai.text-1")}</p>
+            <p>{t("projects.molai.text-2")}</p>
+          </div>
+        </div>
+        <div className={styles.video}>
+          <iframe
+            src="https://www.youtube.com/embed/f5__JJ2mHHk?si=uAxCACyYJCAjteer"
+            title="Molai"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/routes/projects/PitFighters.jsx
+++ src/routes/projects/PitFighters.jsx
@@ -1,1 +1,42 @@
-export default function PitFighters() {}
+import { useTranslation } from "react-i18next";
+
+import styles from "./Project.module.css";
+
+import logo from "../../assets/images/projects/fighters/logo.webp";
+import image1 from "../../assets/images/projects/fighters/image-1.webp";
+import image2 from "../../assets/images/projects/fighters/image-2.webp";
+
+export default function PitFighters() {
+  const { t } = useTranslation();
+  return (
+    <section className={styles.project}>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <div className={styles.titleLeft}>
+            <img src={logo} alt="Pit Fighters Logo" />
+            <h1>{t("projects.fighters.title")}</h1>
+          </div>
+          <div className={styles.buttons}>
+            <a
+              href="https://tecnoboys.itch.io/pit-fighters"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ITCH.IO
+            </a>
+          </div>
+        </div>
+        <div className={styles.info}>
+          <img src={image1} alt={t("projects.fighters.title")} />
+          <div className={styles.text}>
+            <p>{t("projects.fighters.text-1")}</p>
+            <p>{t("projects.fighters.text-2")}</p>
+          </div>
+        </div>
+        <div>
+          <img src={image2} alt="Pit Fighters" className={styles.picture} />
+        </div>
+      </div>
+    </section>
+  );
+}

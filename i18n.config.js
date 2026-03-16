9ce42aba7352f43587f2b663cb9f75fafc95be87
Albert Medina <albertmecu@gmail.com>
2026-03-16 16:10:22 +0100

    feat: setting english as default language

--- i18n.config.js
+++ i18n.config.js
@@ -1,14 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from "react-i18next";
 import locales from "./src/locales.json";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources: locales,
-        fallbackLng: 'en-EN',
+        lng: 'en',
+        fallbackLng: 'en',
         interpolation: {
             escapeValue: false,
         },
